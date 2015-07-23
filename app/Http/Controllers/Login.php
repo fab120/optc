c8441972f7917f382e24fc973bfcd3ac86244e6d
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-23 22:26:40 +0200

    Aggiungo nel database l'utente loggato

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -3,38 +3,52 @@
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Abraham\TwitterOAuth\TwitterOAuth;
+use App\User;
 
 class Login extends BaseController
 {
-    public function login(Request $request)
-    {
+	public function login(Request $request)
+	{
 
-    	$connection = new TwitterOAuth(env('TWITTER_CONSUMER'), env('TWITTER_CONSUMER_SECRET'));
-    	$token = $connection->oauth('oauth/request_token');
+		$connection = new TwitterOAuth(env('TWITTER_CONSUMER'), env('TWITTER_CONSUMER_SECRET'));
+		$token = $connection->oauth('oauth/request_token');
 
-    	$request->session()->put('oauth_token', $token['oauth_token']);
-    	$request->session()->put('oauth_token_secret', $token['oauth_token_secret']);
-    	
-    	return redirect('https://api.twitter.com/oauth/authenticate?oauth_token='.$token['oauth_token']);
-    }
+		$request->session()->put('oauth_token', $token['oauth_token']);
+		$request->session()->put('oauth_token_secret', $token['oauth_token_secret']);
+		
+		return redirect('https://api.twitter.com/oauth/authenticate?oauth_token='.$token['oauth_token']);
+	}
 
-    public function oauth2(Request $request)
-    {
-    	$oauth_token		= $request->session()->get('oauth_token');
-    	$oauth_token_secret	= $request->session()->get('oauth_token_secret');
+	public function oauth2(Request $request)
+	{
+		$oauth_token		= $request->session()->get('oauth_token');
+		$oauth_token_secret	= $request->session()->get('oauth_token_secret');
 
-    	$connection = new TwitterOAuth(env('TWITTER_CONSUMER'), env('TWITTER_CONSUMER_SECRET'), $oauth_token, $oauth_token_secret);
+		$connection = new TwitterOAuth(env('TWITTER_CONSUMER'), env('TWITTER_CONSUMER_SECRET'), $oauth_token, $oauth_token_secret);
 
-    	$oauth_token	= $request->input('oauth_token');
-    	$oauth_verifier	= $request->input('oauth_verifier');
+		$oauth_token	= $request->input('oauth_token');
+		$oauth_verifier	= $request->input('oauth_verifier');
 
-    	$access_token	= $connection->oauth('oauth/access_token', [ "oauth_verifier" => $oauth_verifier ]);
+		$access_token	= $connection->oauth('oauth/access_token', [ "oauth_verifier" => $oauth_verifier ]);
 
-    	$request->session()->put('oauth_token', $access_token['oauth_token']);
-    	$request->session()->put('oauth_token_secret', $access_token['oauth_token_secret']);
-    	$request->session()->put('id', $access_token['user_id']);
+		$request->session()->put('oauth_token', $access_token['oauth_token']);
+		$request->session()->put('oauth_token_secret', $access_token['oauth_token_secret']);
+		$request->session()->put('userid', $access_token['user_id']);
 		$request->session()->put('username', $access_token['screen_name']);
-    	
-    	return redirect('/');
-    }
+
+		$user	= User::where('userid', $access_token['user_id'])->get();
+
+		if(is_null($user)){
+			$user	= new User;
+		}
+
+		$user->oauth_token			= $access_token['oauth_token'];
+		$user->oauth_token_secret	= $access_token['oauth_token_secret'];
+		$user->userid				= $access_token['user_id'];
+		$user->username				= $access_token['screen_name'];
+
+		$user->save();
+		
+		return redirect('/');
+	}
 }
