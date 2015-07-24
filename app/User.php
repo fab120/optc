1f3c40af26b94389e5d0f084b4930f3f62509a8a
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-24 10:52:13 +0200

    implements AuthenticatableContract

--- app/User.php
+++ app/User.php
@@ -1,9 +1,13 @@
 <?php namespace App;
 
 use Jenssegers\Mongodb\Model as Eloquent;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class User extends Eloquent implements AuthenticatableContract {
+class User extends Eloquent implements AuthenticatableContract
+{
+
+	use Authenticatable;
 
     protected $collection = 'users';
 
