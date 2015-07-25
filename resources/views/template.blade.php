abc4149b2a900fcd5b9ecfddc4eed60156590f9c
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-25 15:39:34 +0200

    Aggiunto link twitter

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -66,7 +66,7 @@
 					<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
 					<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
 				</form>
-				<p class="text-muted">Beta version - by fab120</p>
+				<p class="text-muted">Beta version - by <a href="https://twitter.com/fab1200" target="_blank">fab1200</a></p>
 			</div>
 		</footer>
 
