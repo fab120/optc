daae86685cc08b26b6d512cb9b6feeb889a4f40d
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-25 17:35:45 +0200

    Modificata punteggiatura titoli

--- resources/views/website/stats.blade.php
+++ resources/views/website/stats.blade.php
@@ -10,7 +10,7 @@
 			</div>
 			<h3>User using this application:</h3>
 			<p>{{ $user }}</p>
-			<h3>Tweets deleted in last 30 days</h3>
+			<h3>Tweets deleted in last 30 days:</h3>
 			<table class="table table-bordered">
 				<tr>
 					<th>Day</th>
@@ -23,7 +23,7 @@
 					</tr>
 				@endforeach
 			</table>
-			<h3>Total Tweets deleted by this application</h3>
+			<h3>Total Tweets deleted by this application:</h3>
 			<p>{{ $history_total }}</p>
 		</div>
 	</div>
