fb09f19d789a5ec1613630b2e2f163ea3cbaa35c
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-26 13:31:50 +0200

    Aggiorno data corrente in Today

--- app/Http/Controllers/TweetStat.php
+++ app/Http/Controllers/TweetStat.php
@@ -16,6 +16,12 @@ class TweetStat extends BaseController
 		{
 			$data['history']	= DeleteStat::where('user_id',Auth::user()->id)->orderby('data','desc')->take(11)->get();
 			$data['max']		= count($data['history']);
+
+			$nowPST = Carbon::now()->setTimezone('PST')->format('d/m/Y');
+
+			if($data['history'][0]->data->format('d/m/Y') !== $nowPST){
+				array_unshift($data['history'],[ "data" => $nowPST, "count" => 0 ]);
+			}
 		}
 
 		return view('tweetstat.index', $data);
