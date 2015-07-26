5a8afd1e4134bba31d0ed59d3f562dd7df3ceba7
Fabio Cagliero <caglierofabio@gmail.com>
2015-07-26 13:33:43 +0200

    Commentato codice con bug

--- app/Http/Controllers/TweetStat.php
+++ app/Http/Controllers/TweetStat.php
@@ -4,6 +4,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 use App\User;
 use App\DeleteStat;
 use Auth;
+use Carbon\Carbon;
 
 class TweetStat extends BaseController
 {
@@ -17,11 +18,12 @@ class TweetStat extends BaseController
 			$data['history']	= DeleteStat::where('user_id',Auth::user()->id)->orderby('data','desc')->take(11)->get();
 			$data['max']		= count($data['history']);
 
-			$nowPST = Carbon::now()->setTimezone('PST')->format('d/m/Y');
+			/*$nowPST = Carbon::now()->setTimezone('PST')->format('d/m/Y');
 
-			if($data['history'][0]->data->format('d/m/Y') !== $nowPST){
+			if($data['history'][0]->data->format('d/m/Y') !== $nowPST)
+			{
 				array_unshift($data['history'],[ "data" => $nowPST, "count" => 0 ]);
-			}
+			}*/
 		}
 
 		return view('tweetstat.index', $data);
